2288a09c2adb570813d8a2c1229d3f003eded9bf
anna-rose <917566@isd624.org>
2018-07-25 14:04:05 +0000

    automatically backed up by learn

--- lib/curfew_checker.rb
+++ lib/curfew_checker.rb
@@ -1,6 +1,8 @@
 def simple_curfew_checker(time)
-  if time >= 11
-    puts "Past Curfew"
+  if time > 11
+    puts "You're in trouble"
+  else time == 11
+    puts "You're in trouble"
   end 
 end 
 
@@ -10,9 +12,11 @@ simple_curfew_checker(13)
 
 def curfew_checker(time)
   if time > 11
-    puts "Past Curfew!"
-  else time < 11
-    puts "Keep having fun!"
+    puts "You're in trouble"
+  elsif time < 11
+    puts "You can Keep having fun"
+  else time == 11 
+    puts "You're in trouble"
   end
 end 
 
@@ -22,11 +26,11 @@ curfew_checker(13)
 
 def complex_curfew_checker(time)
 if time > 11 
-    puts "Past curfew"
+    puts "You're in trouble"
   elsif time ==11
-    puts "You're in trouble! Better get home quick!"
+    puts "You're in trouble"
   else 
-    puts "Keep having fun!"
+    puts "You can keep having fun"
   end
 end
 
@@ -36,11 +40,11 @@ complex_curfew_checker(12)
 
 def deluxe_curfew_checker(time, curfew = 11)
   if time < curfew
-    puts "You have #{curfew - time} hours!"
+    puts "You can keep having fun. You have #{curfew - time} hours left."
   elsif time - curfew == 0 
-    puts "You're in trouble! Better get home quick!"
+    puts "Time to apparate"
   else time > curfew 
-    puts "Past curfew!"
+    puts "You're in trouble"
   end 
 end
 
@@ -48,16 +52,16 @@ deluxe_curfew_checker(8)
 deluxe_curfew_checker(11)
 deluxe_curfew_checker(13)
 
-def platinum_curfew_checker(current_time, curfew_time)
-  if 
-    puts 
-  elsif 
-    puts
-  else 
-    puts
+def platinum_curfew_checker(current_time, curfew_time = 11)
+  if time == 11
+    puts "You're in trouble"
+  elsif time > 11
+    puts "You're in trouble"
+  else time < curfew 
+    puts "You can keep having fun. You have #{curfew - time} hours left."
   end
 end 
 
-platinum_curfew_checker(8)
+platinum_curfew_checker(9)
 platinum_curfew_checker(11)
 platinum_curfew_checker(13)
